f75ab1d78e81df5ae150cbcb976c898b13ce6273
Adriana Rangel <adri.rangel6@gmail.com>
2023-10-25 19:15:44 -0700

    2 images on homepage

--- front/js/script.js
+++ front/js/script.js
@@ -12,15 +12,26 @@ function displayProducts(products) {
     console.log(products);
     const items = document.getElementById('items');
     // TODO use javascript loop to display all parts
-    const product= products[0];
+    const product = products[0];
     items.innerHTML += `
           <a href="./product.html?id=42">
             <article>
-              <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <img src="${product.imageUrl}" alt="Photo of a blue sofa, two seats">
               <h3 class="productName">${product.name}</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+              <p class="productDescription">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </article>
           </a>
     `
 }
 
+
+const images = ["image"];
+const imageSource = "${product.imageUrl}";
+
+const homepageImages = document.getElementById('items');
+
+for (let i = 0; i < images.length; i = 0++); {
+    const img = document.createElement('img');
+    img.src = "${product.imageUrl}";
+    homepageImages.appendChild(img);
+}
