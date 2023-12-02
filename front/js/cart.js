51fb6a01a04d8a43dc5df2f9dd36198d2ccbce86
Adriana Rangel <adri.rangel6@gmail.com>
2023-12-01 20:55:48 -0800

    cart page finished

--- front/js/cart.js
+++ front/js/cart.js
@@ -21,21 +21,21 @@ fetch(`http://localhost:3000/api/products/${cartItem.productId}`)
 function displayProducts(product) {
   console.log(cart);
   const cartElement = document.getElementById('cart__items');
-  cartElement.innerHTML = `
-  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+  cartElement.innerHTML += `
+  <article class="cart__item" data-id="{productId}" data-color="{product-color}">
   <div class="cart__item__img">
     <img src="${product.imageUrl}" alt="${product.altTxt}">
   </div>
   <div class="cart__item__content">
     <div class="cart__item__content__description">
-      <h2>${product.name}</h2>
-      <p>${product.colors}</p>
-      <p>${product.price}</p>
+      <h2>"${product.name}"</h2>
+      <p>"${product.colors}"</p>
+      <p>"${product.price}"</p>
     </div>
     <div class="cart__item__content__settings">
       <div class="cart__item__content__settings__quantity">
-        <p>Quantity :${product.quantity}</p>
-        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+        <p>Quantity : </p>
+        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartItem.quantity}">
       </div>
       <div class="cart__item__content__settings__delete">
         <p class="deleteItem">Delete</p>
@@ -45,5 +45,6 @@ function displayProducts(product) {
 </article>
 `
 ;
-}}
+}
+}
   
