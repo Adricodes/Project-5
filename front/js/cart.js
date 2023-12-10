ae8010af3cbe514151d27537f778eb882501607d
Adriana Rangel <adri.rangel6@gmail.com>
2023-12-09 19:16:32 -0800

    total price review

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,50 +1,73 @@
 const cartElement = document.getElementById('cart__items');
-
+const totalQuantityElement = document.getElementById('totalQuantity');
+const currentTotalPrice = document.getElementById('totalPrice');
 const cart = JSON.parse(localStorage.getItem('cart') || "[]");
 console.log(cart);
-// TODO iterate over the cart and look at each item inside of it
+
 for (let i = 0; i < cart.length; i++) {
   const cartItem = cart[i];
   console.log(cartItem.productId)
 
-// TODO inside the for loop grab product id for the current cart item and fetch its product from the backend using fetch API
-//     and use the information for this cart item along with its product information to insert a card in the page
+  fetch(`http://localhost:3000/api/products/${cartItem.productId}`)
+    .then(data => {
+      return data.json();
+    })
+    .then(product => {
+      displayProducts(product, cartItem);
+    });
+}
 
-fetch(`http://localhost:3000/api/products/${cartItem.productId}`)
-  .then(data => {
-    return data.json();
-  })
-  .then(product => {
-    displayProducts(product);
-  });
-
-function displayProducts(product) {
+function displayProducts(product, cartItem) {
   console.log(cart);
-  const cartElement = document.getElementById('cart__items');
+  // FIXME change curly brace productID to cartProductId
+  // FIXME change curly brace other color to cartItem color
   cartElement.innerHTML += `
-  <article class="cart__item" data-id="{productId}" data-color="{product-color}">
-  <div class="cart__item__img">
-    <img src="${product.imageUrl}" alt="${product.altTxt}">
+<article class="cart__item" data-id=${cartItem.productId} data-color=${cartItem.color}>
+<div class="cart__item__img">
+  <img src=${product.imageUrl} alt="${product.altTxt}">
+</div>
+<div class="cart__item__content">
+  <div class="cart__item__content__description">
+    <h2>${product.name}</h2>
+    <p>${cartItem.color}</p>
+    <p>€${product.price}</p>
   </div>
-  <div class="cart__item__content">
-    <div class="cart__item__content__description">
-      <h2>"${product.name}"</h2>
-      <p>"${product.colors}"</p>
-      <p>"${product.price}"</p>
+  <div class="cart__item__content__settings">
+    <div class="cart__item__content__settings__quantity">
+      <p>Quantity : </p>
+      <input type="number" class="itemQuantity" 
+          name="itemQuantity" min="1" max="100" 
+          value="${cartItem.quantity}">
     </div>
-    <div class="cart__item__content__settings">
-      <div class="cart__item__content__settings__quantity">
-        <p>Quantity : </p>
-        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartItem.quantity}">
-      </div>
-      <div class="cart__item__content__settings__delete">
-        <p class="deleteItem">Delete</p>
-      </div>
+    <div class="cart__item__content__settings__delete">
+      <p class="deleteItem">Delete</p>
     </div>
   </div>
+</div>
 </article>
 `
-;
+  // TODO update total quantity on webpage
+  const currentTotalQuantity = parseInt(totalQuantityElement.innerText || 0);
+  totalQuantityElement.innerText = cartItem.quantity + currentTotalQuantity;
+
+  const totalPrice = parseInt(currentTotalPrice.innerText || 0);
+  currentTotalPrice.innerText = product.price * totalPrice;
+
+  // price for cart items
+
+
+  // Get the total and display it
+
+
+  // You can then display the total in your HTML or use it as needed
+
+
+
+  // first thing: where is the current price coming from on the page? 
+  // And if it is an empty string, what should the value be instead so 
+  // that we can add the first cart item price to it?
+
+
+
+
 }
-}
-  
